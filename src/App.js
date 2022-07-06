a095535942f4b2d00cf64ce2deadb93eb03fdfe4
matthewblu421 <44147556+matthewblu421@users.noreply.github.com>
2022-07-06 16:52:55 -0400

    started my first react project! resume parser for google company case

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ReactDOM from 'react-dom';
+import PostForm from './components/PostForm'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1 className="main-header">Matt's Resume Parser</h1>     
+     <Upload />
+     <PostForm />
     </div>
+  );
+}
+
+export function Upload() {
+  return (
+      <div className='upload-file-container'>
+      <h3>
+        CV/Resume
+      </h3>
+      <button className='upload-button'>Upload</button>
+      </div>
+
   );
 }
 
